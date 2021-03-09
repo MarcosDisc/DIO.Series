dab9e1ba5f4fc369d98b74c0c8209f65123ebf4f
MarcosDisc <marcosdisc@gmail.com>
2021-03-09 11:28:45 -0300

    Terminado...

--- Classes/Serie.cs
+++ Classes/Serie.cs
@@ -1,3 +1,4 @@
+using System;
 using Series.Enum;
 
 namespace Series.Classes
@@ -9,6 +10,7 @@ namespace Series.Classes
         private string Titulo { get; set; }
         private string Descricao { get; set; }
         private int Ano { get; set; }
+        private bool Excluido { get; set; }
 
         public Serie(int id, Genero genero, string titulo, string descricao, int ano)
         {
@@ -17,10 +19,40 @@ namespace Series.Classes
             this.Titulo = titulo;
             this.Descricao = descricao;
             this.Ano = ano;
+            this.Excluido = false;
         }
+
+        public override string ToString()
+        {
+            string retorno = "";
+            retorno += "Gênero: " + this.Genero + Environment.NewLine;
+            retorno += "Título: " + this.Titulo + Environment.NewLine;
+            retorno += "Descrição: " + this.Descricao + Environment.NewLine;
+            retorno += "Ano de Início: " + this.Ano + Environment.NewLine;
+            retorno += "Excluído: " + this.Excluido;
+            return base.ToString();
+        }
+
+        public string retornaTitulo() 
+        {
+            return this.Titulo;
+        }
+
+        public int retornaId()
+        {
+            return this.Id;
+        }
+
+        public void Excluir()
+        {
+            this.Excluido = true;
+        }
+
+        public bool retornaExcliodo()
+        {
+            return this.Excluido;
+        }
+
+
     }
-
-
-
-
 }
